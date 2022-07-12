e56a39ca3a9f548f736a130b353e84940f86f93b
ahmadsawaed99 <ahmadsawaed.99@outlook.com>
2022-07-12 22:23:03 +0300

    change

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
+import { PageNotFoundComponent } from "../components/page-not-found/page-not-found.component";
 import { StudentCoursesComponent } from "../components/student-courses/student-courses.component";
 import { StudentsClassesComponent } from "../components/students-classes/students-classes.component";
 
 @NgModule({
   declarations : [
     StudentCoursesComponent,
-    StudentsClassesComponent
+    StudentsClassesComponent,
+    PageNotFoundComponent
   ]
 })
 export class StudentModule{}
